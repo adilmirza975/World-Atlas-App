799b0b37e60020c616826b78bf16d0e214ab4053
ADIL  MIRZA <adil.mirza2024@nst.rishihood.edu.in>
2025-07-01 23:13:46 +0530

    contact page done

--- src/components/UI/Footers.jsx
+++ src/components/UI/Footers.jsx
@@ -1,3 +1,63 @@
+// i am using custom api footerApi.json
+
+import footerContact from "../../api/footerApi.json";
+import { NavLink } from "react-router-dom";
+
+// icons import 
+import { MdPlace } from "react-icons/md";
+import { IoCallSharp } from "react-icons/io5";
+import { TbMailPlus } from "react-icons/tb";
+
 export const Footers = () => {
-  return <h1>Footer</h1>
-}
+
+  const footerIcon = {
+    MdPlace: <MdPlace />,
+    IoCallSharp: <IoCallSharp/>,
+    TbMailPlus: <TbMailPlus/>
+
+  }
+
+  return (
+    <footer className="footer-section">
+      <div className="container grid grid-three-cols">
+
+        {footerContact.map((curData, index) => {
+
+          // destructure so that i dont have to use curData.icon or anything else ,just icon
+          const { icon, title, details } = curData;
+          return (
+            <div className="footer-contact" key={index}>
+              <div className="icon">{footerIcon[icon]}</div>
+              <div className="footer-contact-text">
+              <p>{title}</p>
+              <p>{details}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="copyright-area">
+        <div className="container">
+          <div className="grid grid-two-cols">
+
+            <div className="copyright-text">
+              <p>
+                copyright &cpoy; 2024, All Right Reserved 
+              </p>
+            </div>
+
+             <div className="footer-menu">
+              <ul>
+                <li>
+                    <NavLink to="https://vellavibe.com/" target="_blank">My Clothing Brand - VELLAVIBE</NavLink>
+                </li>
+              </ul>
+             </div>
+
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
